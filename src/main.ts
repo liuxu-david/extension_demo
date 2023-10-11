765adcfc9e91a8f733bb062eb13fe82ac8669ce6
liuxu <2982359231@qq.com>
2023-10-11 20:52:32 +0800

    添加接口文档配置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('项目名称')
+    .setDescription('文档描述')
+    .setVersion('版本号')
+    .addTag('文档标签')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
